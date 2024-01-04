219abf29d08c4f283dda50aa512d254c975dfd57
avantikasparihar <avantikasparihar@gmail.com>
2024-01-04 18:55:44 +0530

    data layer bootstrapped for book object

---
+++ src/data.py
@@ -0,0 +1,48 @@
+
+class Book:
+    def __init__(self, id, title, total_pages, percent_finished_prev, percent_finished_now):
+        self.id = id
+        self.title = title
+        self.total_pages = total_pages
+        self.percent_finished_prev = percent_finished_prev
+        self.percent_finished_now = percent_finished_now
+
+book1 = Book(1,"The Magic of Reality", 270, 35, 35)
+book2 = Book(2,"Fundamentals of Software Architecture", 419, 18, 28)
+
+currently_reading = [book1, book2]
+
+def get_book(id):
+    """Return information about a specific book."""
+    for book in currently_reading:
+        if book.id == id:
+            return book
+    print("Book not found.")
+
+def list_books():
+    return currently_reading
+
+def create_book(title, total_pages, percent_finished_prev, percent_finished_now):
+    """Create a new book and add it to the list."""
+    book = Book(title, total_pages, percent_finished_prev, percent_finished_now)
+    currently_reading.append(book)
+    return book
+
+def update_book(title, new_total_pages, new_percent_finished_now):
+    """Update information about a specific book."""
+    book = get_book(title)
+    if book:
+        book.total_pages = new_total_pages
+        book.percent_finished_now = new_percent_finished_now
+        print("Book updated successfully.")
+        return book
+    print("Book not found.")
+
+def delete_book(title):
+    """Delete a specific book from the list."""
+    book = get_book(title)
+    if book:
+        currently_reading.remove(book)
+        print("Book deleted successfully.")
+        return
+    print("Book not found.")
--- src/reading_cli.py
+++ src/reading_cli.py
@@ -1,33 +1,40 @@
 # cli.py
-import click
 import datetime
+import os
+import click
 
-@click.command()
-@click.option('--name', prompt='Your name', help='The person to greet.')
+import data
+
+@click.group("reading-cli")
+def reading_cli():
+    pass
+
+@reading_cli.command("greet")
+@click.option('--name', required=True, help='The person to greet.')
 def hello(name):
-    click.echo(f"Hello {name}! Today is {datetime.date.today()}\n")
+    current_user = os.getlogin()
+    click.echo(f"Hello {name}! \nusername: {current_user} \nToday is {datetime.date.today()}\n")
 
-@click.command()
+@reading_cli.command("show-current")
 def currently_reading():
-    currently_reading_books = ["the magic of reality", "fundamentals of software architecture"]
-    total_pages = [270, 419]
-    percent_finished_prev = [35, 18]
-    percent_finished_now = [35, 28]
+    books = data.list_books()
 
     click.echo("You're currently reading: ")
-    for i, book in enumerate(currently_reading_books):
-        click.echo(f"{i+1}. {book} - {total_pages[i]} pages, {percent_finished_now[i]}% complete")
+    for i, book in enumerate(books):
+        click.echo(f"{i+1}. {book.title} - {book.total_pages} pages, {book.percent_finished_now}% complete")
 
     click.echo("\n")
     total_pages_read = 0
 
-    for i in range(len(total_pages)):
-        pages_read = total_pages[i] * (percent_finished_now[i] - percent_finished_prev[i]) / 100
-        total_pages_read += pages_read
-        click.echo(f"Pages read for {currently_reading_books[i]}: {pages_read}")
+    # for i in range(len(total_pages)):
+    #     pages_read = total_pages[i] * (percent_finished_now[i] - percent_finished_prev[i]) / 100
+    #     total_pages_read += pages_read
+    #     click.echo(f"Pages read for {currently_reading_books[i]}: {pages_read}")
 
     click.echo(f"\nYou've read {total_pages_read} pages today")
 
+def main():
+    reading_cli()
+
 if __name__ == '__main__':
-    hello()
-    currently_reading()
+    main()
