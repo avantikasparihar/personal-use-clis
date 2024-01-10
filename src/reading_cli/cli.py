47015fefb82d608c6ec3b6b5897d7e012e7e5297
avantikasparihar <avantikasparihar@gmail.com>
2024-01-11 01:16:04 +0530

    defined abstract func defs and default impl to manage books

--- src/reading_cli/cli.py
+++ src/reading_cli/cli.py
@@ -3,7 +3,9 @@ import datetime
 import os
 import click
 
-from data.data import get_book, list_books
+from data.default import DefaultBooksManager
+
+books_manager = DefaultBooksManager()
 
 @click.group("reading-cli")
 def reading_cli():
@@ -19,11 +21,11 @@ def hello(name):
 @click.option('--id', required=False, help='ID of the book to show.')
 def currently_reading(id):
     if id != None:
-        book = get_book(id)
+        book = books_manager.get_book(id)
         click.echo(f"{book.id}. {book.title} - {book.total_pages} pages, {book.percent_finished_now}% complete")
         return
 
-    books = list_books()
+    books = books_manager.list_books()
 
     click.echo("You're currently reading: ")
     for i, book in enumerate(books):
@@ -33,12 +35,12 @@ def currently_reading(id):
 @click.option('--id', required=False, help='ID of the book to show.')
 def total_pages(id):
     if id != None:
-        book = get_book(id)
+        book = books_manager.get_book(id)
         pages_read = book.total_pages * (book.percent_finished_now - book.percent_finished_prev) / 100
         click.echo(f"You've read {pages_read} pages of {book.title} today")
         return
 
-    books = list_books()
+    books = books_manager.list_books()
     total_pages_read = 0
 
     for i, book in enumerate(books):
