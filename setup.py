0fb974b74965a265ac134d4e56fe7c3bcf6f213f
avantikasparihar <avantikasparihar@gmail.com>
2024-01-14 14:46:16 +0530

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="personal-use-clis",
+    name="utility-clis",
     version="0.1",
     packages=find_packages(),
     install_requires=[
         'click==8.0.3',
+        'PyYAML==6.0.1'
     ],
     entry_points={
         'console_scripts': [
             'hello-cli=src.hello_cli.cli:hello',
-            'reading-cli=src.reading_cli.cli:reading_cli'
+            'read-100pages-a-day-cli=src.reading_cli.cli:reading_cli'
         ],
     },
 )
